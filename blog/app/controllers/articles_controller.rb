97600e300365bcb5130ca6d4032205beaae67f60
Richard <pichipichi310@hotmail.com>
2015-12-18 12:52:05 +0100

    terminado versión básica

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,7 +2,8 @@ class ArticlesController < ApplicationController
 	def new
 	  @article = Article.new
 	end
-
+	
+	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
 	def index
 	    @articles = Article.all
